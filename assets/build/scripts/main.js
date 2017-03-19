5a92dc52efef66423bbe5ce59297e900293f5983
IvankivRLCoder <hyphnotic1@gmail.com>
2017-03-19 21:47:39 +0200

    Add new scss file(popup) add input

--- assets/build/scripts/main.js
+++ assets/build/scripts/main.js
@@ -8,3 +8,57 @@ app.controller('userCtrl', function($scope, $http) {
         $scope.users = response;
     });
 });
+
+app.controller('AppController', ['$scope', function($scope) {
+    var self = this;
+    self.user = { id: null, username: '', email: '', password: '', number: '', country: '', city: '' };
+    self.id = 4;
+
+    self.users = [];
+
+    self.submit = function() {
+        if (self.user.id === null) {
+            self.user.id = self.id++;
+            console.log('Saving New User', self.user);
+            self.users.push(self.user);
+        } else {
+            for (var i = 0; i < self.users.length; i++) {
+                if (self.users[i].id === self.user.id) {
+                    self.users[i] = self.user;
+                    break;
+                }
+            }
+            console.log('User updated with id ', self.user.id);
+        }
+        self.reset();
+    };
+
+    self.edit = function(id) {
+        console.log('id to be edited', id);
+        for (var i = 0; i < self.users.length; i++) {
+            if (self.users[i].id === id) {
+                self.user = angular.copy(self.users[i]);
+                break;
+            }
+        }
+    }
+
+    self.remove = function(id) {
+        console.log('id to be deleted', id);
+        for (var i = 0; i < self.users.length; i++) {
+            if (self.users[i].id === id) {
+                self.users.splice(i, 1);
+                if (self.user.id === id) {
+                    self.reset();
+                }
+                break;
+            }
+        }
+    }
+
+    self.reset = function() {
+        self.user = { id: null, username: '', email: '', password: '', number: '', country: '', city: '' };
+        $scope.myForm.$setPristine();
+    }
+
+}]);
